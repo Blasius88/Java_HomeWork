873c236c484d6d6fda75f66d7230b811bfde7605
Roman Dolbik-Vorobey <dolbik-w.by@mail.ru>
2019-09-01 15:21:10 +0300

    Домашняя работа

--- src/Lector5/Lector5.java
+++ src/Lector5/Lector5.java
@@ -1,4 +1,22 @@
 package Lector5;
 
+import java.util.Scanner;
+
 public class Lector5 {
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 1 число");
+        int a = sc.nextInt();
+        System.out.println("Введите 2 число");
+        int b = sc.nextInt();
+        int sum = Task.task(a, b);
+        System.out.println("Введите 1 слово");
+        String one = sc.next();
+        System.out.println("Введите 2 слово");
+        String two = sc.next();
+        String str = Task.task(one, two);
+        System.out.println("Sum = " + sum);
+        System.out.println("Str =" + str);
+    }
 }
---
+++ src/Lector5/Task.java
@@ -0,0 +1,14 @@
+package Lector5;
+
+public class Task {
+    public static String task (String one, String two)
+    {
+        String str = one + " " + two;
+        return str;
+    }
+    public static int task (int a, int b)
+    {
+        int c = a+b;
+        return c;
+    }
+}
