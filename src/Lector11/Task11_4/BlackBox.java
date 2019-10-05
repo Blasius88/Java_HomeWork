e8e4c3313ef19b2d78f9fe20f5918bbea9a10e30
Roman Dolbik-Vorobey <dolbik-w.by@mail.ru>
2019-10-05 03:55:48 +0300

    BlackBox исправлен

---
+++ src/Lector11/Task11_4/BlackBox.java
@@ -0,0 +1,51 @@
+package Lector11.Task11_4;
+
+import java.util.*;
+
+public class BlackBox {
+    private List<Integer> numbers;
+    BlackBox()
+    {
+        numbers = new ArrayList<>();
+    }
+
+    void addToBox(Integer num)
+    {
+        numbers.add(num);
+    }
+
+    List<Integer> getNumbers()
+    {
+        return numbers;
+    }
+
+    Integer min(int k)
+    {
+        Set<Integer> set = new HashSet<>();
+        set.addAll(numbers);
+        numbers.clear();
+        numbers.addAll(set);
+        Collections.sort(numbers);
+
+        if (k>0 && k<=numbers.size()){
+            return numbers.get(k-1);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    Integer max(int n)
+    {
+        Set<Integer> set = new HashSet<>();
+        set.addAll(numbers);
+        numbers.clear();
+        numbers.addAll(set);
+        Collections.sort(numbers);
+        Collections.reverse(numbers);
+        if (n>0 && n<=numbers.size()){
+            return numbers.get(n-1);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
--- src/Lector11/Task11_4/MainTask11_4.java
+++ src/Lector11/Task11_4/MainTask11_4.java
@@ -4,47 +4,19 @@ import java.util.*;
 
 public class MainTask11_4 {
     public static void main(String[] args) {
-        blackBox();
-    }
-    private static void blackBox() {
-        TreeSet<Integer> treeSet = new TreeSet<>();
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            treeSet.add(random.nextInt(100) + 1);
-        }
-        ArrayList<Integer> blackBox = new ArrayList<>();
-        blackBox.addAll(treeSet);
-        System.out.println(blackBox);
-        System.out.println(kMin(blackBox, 2));
-        System.out.println(nMax(blackBox, 4));
-    }
-    private static Integer nMax(ArrayList<Integer> blackBox, int n) {
-        ListIterator iterator = blackBox.listIterator();
-        while(iterator.hasNext()){
-            iterator.next();
-        }
-        int nx = blackBox.size();
-        n=nx-n;
-        while (iterator.hasPrevious()) {
-            if (nx == n) {
-                return (Integer) iterator.previous();
-            }
-            iterator.previous();
-            nx--;
-        }
-        return 0;
-    }
+        BlackBox box = new BlackBox();
+        box.addToBox(10);
+        box.addToBox(10);
+        box.addToBox(30);
+        box.addToBox(50);
+        box.addToBox(50);
 
-    private static Integer kMin(ArrayList<Integer> blackBox, int k) {
-        Iterator iterator = blackBox.iterator();
-        int kx = 0;
-        while (iterator.hasNext()) {
-            if (kx == k) {
-                return (Integer) iterator.next();
-            }
-            iterator.next();
-            kx++;
-        }
-        return 0;
+        System.out.println(box.getNumbers());
+
+        System.out.println(box.min(2));
+
+        System.out.println(box.max(2));
+
+
     }
 }
